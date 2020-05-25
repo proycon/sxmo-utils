ce5c5297387ef501b72fab5e41f24283666e1186
Miles Alan <m@milesalan.com>
2020-05-25 00:56:52 -0500

    Fix bug with camera error message popping up after usage

--- scripts/core/sxmo_camera.sh
+++ scripts/core/sxmo_camera.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 err() {
   echo -e "$1" | dmenu -fn Terminus-20 -c -l 10
+  exit 1
 }
 
-media-ctl -d /dev/media1 --set-v4l2 '"ov5640 3-004c":0[fmt:UYVY8_2X8/1280x720]'
-mpv --video-rotate=90 av://v4l2:/dev/video1 || err "Couldn't open camera, is camera enabled?"
+media-ctl -d /dev/media1 --set-v4l2 '"ov5640 3-004c":0[fmt:UYVY8_2X8/1280x720]' || err "Couldn't open camera, is camera enabled?"
+mpv --video-rotate=90 av://v4l2:/dev/video1
