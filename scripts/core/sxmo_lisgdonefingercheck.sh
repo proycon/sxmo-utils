b80ce9e8726b57e991e033cca727668892aa965d
Miles Alan <m@milesalan.com>
2020-06-26 20:02:24 -0500

    Disable ligsd 1 finger gestures while using foxtrotgps

---
+++ scripts/core/sxmo_lisgdonefingercheck.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+ACTIVEWIN="$(xdotool getactivewindow)"
+WMCLASS="$(xprop -id "$ACTIVEWIN" | grep WM_CLASS | cut -d ' ' -f3-)"
+
+# E.g. just a check to ignore 1-finger gestures in foxtrotgps
+if echo "$WMCLASS" | grep -vi foxtrot; then
+  $@
+fi
--- scripts/core/sxmo_lisgdstart.sh
+++ scripts/core/sxmo_lisgdstart.sh
@@ -2,12 +2,12 @@
 pkill -9 lisgd
 
 lisgd $@ \
-	-g '1,LR,xdotool key --clearmodifiers Alt+Shift+e' \
-	-g '1,RL,xdotool key --clearmodifiers Alt+Shift+r' \
-	-g '1,DLUR,sxmo_vol.sh up' \
-	-g '1,URDL,sxmo_vol.sh down' \
-	-g '1,DRUL,sxmo_brightness.sh up' \
-	-g '1,ULDR,sxmo_brightness.sh down' \
+	-g '1,LR,sxmo_lisgdonefingercheck.sh xdotool key --clearmodifiers Alt+Shift+e' \
+	-g '1,RL,sxmo_lisgdonefingercheck.sh xdotool key --clearmodifiers Alt+Shift+r' \
+	-g '1,DLUR,sxmo_lisgdonefingercheck.sh sxmo_vol.sh up' \
+	-g '1,URDL,sxmo_lisgdonefingercheck.sh sxmo_vol.sh down' \
+	-g '1,DRUL,sxmo_lisgdonefingercheck.sh sxmo_brightness.sh up' \
+	-g '1,ULDR,sxmo_lisgdonefingercheck.sh sxmo_brightness.sh down' \
 	-g '2,LR,xdotool key --clearmodifiers Alt+e' \
 	-g '2,RL,xdotool key --clearmodifiers Alt+r' \
 	-g '2,DU,pidof svkbd-sxmo || svkbd-sxmo &' \
