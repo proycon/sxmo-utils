547c97f2264443ff56a3839ac0459a83ba5aafd7
Maarten van Gompel <proycon@anaproy.nl>
2020-07-27 00:23:28 +0200

    Implementing optional rtc wakeup at regular intervals, added presuspend, rtcwake and postwake scripts (with hooks)

---
+++ scripts/core/sxmo_postwake.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# This script is called when the system has successfully woken up after sleep
+
+sxmo_statusbarupdate.sh
+
+if [ -x "$XDG_CONFIG_HOME/sxmo/hooks/postwake" ]; then
+    "$XDG_CONFIG_HOME/sxmo/hooks/postwake"
+fi
