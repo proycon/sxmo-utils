36535ebfe30a505927fedec469759e9675c00a86
Maarten van Gompel <proycon@anaproy.nl>
2021-03-28 21:24:50 +0200

    added another layer between the power button press and the keyboard script, with option to override using a hook

---
+++ scripts/core/sxmo_powerbutton.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# include common definitions
+# shellcheck source=scripts/core/sxmo_common.sh
+. "$(dirname "$0")/sxmo_common.sh"
+
+if [ -x "$XDG_CONFIG_HOME/sxmo/hooks/powerbutton" ]; then
+	"$XDG_CONFIG_HOME/sxmo/hooks/powerbutton" "$@"
+else
+	sxmo_keyboard.sh "$@"
+fi
