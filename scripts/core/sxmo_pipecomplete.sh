0b79710cb5212418091134a6210da46758f289f4
Stacy Harper <contact@stacyharper.net>
2021-05-23 14:25:02 +0200

    Fix and simplify the pipecomplete svkbd managing

--- scripts/core/sxmo_pipecomplete.sh
+++ scripts/core/sxmo_pipecomplete.sh
@@ -3,14 +3,12 @@ INPUT="$(cat)"
 STWIN="$(xprop -root | sed -n '/^_NET_ACTIVE_WINDOW/ s/.* //p')"
 
 menu() {
-	sxmo_keyboard.sh open
 	RESULT="$(
 		printf %b "$(
 		echo "Close Menu";
 			echo "$INPUT" | grep -Eo '\S+' | tr -d '[:blank:]' | sort | uniq
-		)" | dmenu -p "$PROMPT" -l 10 -i -c
+		)" | sxmo_dmenu_with_kb.sh -p "$PROMPT" -l 10 -i -c
 	)"
-	sxmo_keyboard.sh close
 }
 
 copy() {
