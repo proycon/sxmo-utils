c5c5537873c7ba6c8a8af0169c8f875f6140fc5e
Stacy Harper <contact@stacyharper.net>
2021-06-06 09:20:50 +0200

    Do not enforce -e TERMCMD args and fix TERMMODE
    Enforcing -e break some possibilities to use TERMCMD that dont use it.
    
    TERMCMD should be setup in xinit. This way, in ssh mode, the value still
    is empty. This way, some custom bash or tmux config could use
    it, configuring it.

--- scripts/core/sxmo_terminal.sh
+++ scripts/core/sxmo_terminal.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env sh
 
-# You can export a different terminal in your xinit
-TERMCMD="${TERMCMD:-st}"
-TERMMODE=$([ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] && echo "true")
-
-if [ "$TERMMODE" = "true" ]; then
-	while [ "-e" != "$1" ] || [ 0 -eq $# ]; do
-		shift
-	done
-	shift
-else
-	set -- "$TERMCMD" -e "$@"
-fi
+# shellcheck disable=SC2086
+set -- $TERMCMD "$@"
 
 if [ -z "$*" ]; then
 	echo "sxmo_terminal.sh called in TERMMODE without any arguments (returning, nothing to do)" >&2
