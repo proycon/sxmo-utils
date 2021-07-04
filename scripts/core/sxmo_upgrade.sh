672566c20672113e5b1e271b176ea0e13c315ea6
Stacy Harper <contact@stacyharper.net>
2021-07-04 10:28:19 +0200

    Remove -a cause it can break the system.
    If the apk cache is outdated (first installation) and you try to upgrade
    with this "-a" argument without configuring a network, then apk will
    just strip every packages that are not available.
    
    It then leave the phone in a very broken state.
    
    The -a was used to handle mainlining software from pmaport to aport
    (where pmaport uses 9999 version numbers). As we do not rely that much
    on those softwares (that are now mainlined), we strip this unsafe
    argument.
    
    Signed-off-by: Stacy Harper <contact@stacyharper.net>
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

--- scripts/core/sxmo_upgrade.sh
+++ scripts/core/sxmo_upgrade.sh
@@ -7,7 +7,7 @@ echo "Updating all packages from repositories"
 sudo apk update
 
 echo "Upgrading all packages"
-sudo apk upgrade -a
+sudo apk upgrade
 
 echo "Upgrade complete - reboot for all changes to take effect"
 read -r
