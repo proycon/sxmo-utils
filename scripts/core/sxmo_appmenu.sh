1d9821c2a403e2f7cad85e428353254082a3b19c
Galen Abell <galen@galenabell.com>
2021-07-10 16:08:33 +0200

    Don't echo pmos-tweaks path in config menu
    Signed-off-by: Maarten van Gompel <proycon@anaproy.nl>

--- scripts/core/sxmo_appmenu.sh
+++ scripts/core/sxmo_appmenu.sh
@@ -151,7 +151,7 @@ programchoicesinit() {
 			$icon_ror Rotate                     ^ 1 ^ sxmo_rotate.sh rotate
 			$icon_upc Upgrade Pkgs               ^ 0 ^ sxmo_terminal.sh sxmo_upgrade.sh
 			$icon_cfg Edit configuration         ^ 0 ^ sxmo_terminal.sh $EDITOR $XDG_CONFIG_HOME/sxmo/xinit
-			$(command -v pmos-tweaks && echo "$icon_cfg PostmarketOS Tweaks	     ^ 0 ^ GDK_SCALE=1 pmos-tweaks")
+			$(command -v pmos-tweaks >/dev/null && echo "$icon_cfg PostmarketOS Tweaks	     ^ 0 ^ GDK_SCALE=1 pmos-tweaks")
 		"
 		WINNAME=Config
 		;;
