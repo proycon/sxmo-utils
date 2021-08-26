b94c16540f41008d05b11fb46e448b8f21c44790
Stacy Harper <contact@stacyharper.net>
2021-08-26 12:13:27 +0200

    Switch wm without rebooting
    Signed-off-by: Stacy Harper <contact@stacyharper.net>
    Signed-off-by: Maarten van Gompel <proycon@anaproy.nl>

--- scripts/core/sxmo_wmtoggle.sh
+++ scripts/core/sxmo_wmtoggle.sh
@@ -3,10 +3,10 @@
 case "$(realpath /var/lib/tinydm/default-session.desktop)" in
 	/usr/share/wayland-sessions/swmo.desktop)
 		sudo tinydm-set-session -f -s /usr/share/xsessions/sxmo.desktop
+		pkill sway
 		;;
 	/usr/share/xsessions/sxmo.desktop)
 		sudo tinydm-set-session -f -s /usr/share/wayland-sessions/swmo.desktop
+		pkill dwm
 		;;
 esac
-
-sudo reboot
