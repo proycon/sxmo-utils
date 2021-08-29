2f0eb4ffe6c05f453fcc190e53f6b272a6de5e10
Maxim Karasev <begs@disroot.org>
2021-08-29 14:28:37 +0300

    Drop "BATTERY_DEVICE" property in deviceprofiles
    Isn't needed anymore as we now have reliable and universal battery
    autodetection.
    
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/deviceprofiles/sxmo_deviceprofile_hammerheadqcom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Nexus 5
-
-export BATTERY_DEVICE="/sys/class/power_supply/battery"
--- scripts/deviceprofiles/sxmo_deviceprofile_l8150qcom.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_l8150qcom.sh
@@ -2,8 +2,6 @@
 
 # Wileyfox Swift (wileyfox-crackling)
 
-export BATTERY_DEVICE="/sys/class/power_supply/pm8916-bms-vm"
-
 export TOUCH_POINTER_ID="10"
 
 export ROTATION_GRAVITY="500"
