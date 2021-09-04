a5799bf3510d99513414d1bcd96279aa50a9e20a
Stacy Harper <contact@stacyharper.net>
2021-09-04 10:43:01 +0200

    Add a check before toggeling wm
    Currently on 1.5.1, sway is not installed nor setted up. This WM toggle
    entry cause a tty soft lock !
    
    This fix this with a very too simple check but that will prevent this
    case.

--- scripts/core/sxmo_wmtoggle.sh
+++ scripts/core/sxmo_wmtoggle.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+not_ready_yet() {
+	notify-send "Your device looks not ready yet"
+	exit 1
+}
+
 case "$(realpath /var/lib/tinydm/default-session.desktop)" in
 	/usr/share/wayland-sessions/swmo.desktop)
+		command -v dwm || not_ready_yet
 		sudo tinydm-set-session -f -s /usr/share/xsessions/sxmo.desktop
 		pkill sway
 		;;
 	/usr/share/xsessions/sxmo.desktop)
+		command -v sway || not_ready_yet
 		sudo tinydm-set-session -f -s /usr/share/wayland-sessions/swmo.desktop
 		pkill dwm
 		;;
