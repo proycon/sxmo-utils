23391c1e3ef1ff470b8338296389853e0d2636af
Anjandev Momi <anjan@momi.ca>
2021-10-20 02:45:57 -0400

    Add screenshot support in wayland (sway)
    User must have grim and slurp installed.
    We should add those programs to the swmo ui metapackage
    
    Signed-off-by: Maarten van Gompel <proycon@anaproy.nl>

--- scripts/appscripts/sxmo_screenshot.sh
+++ scripts/appscripts/sxmo_screenshot.sh
@@ -2,8 +2,25 @@
 # scrot refuses to work with double quotes
 # shellcheck disable=SC2016
 
+
+case "$(sxmo_wm.sh)" in
+	sway)
+		FILENAME="$(date +%Y-%m-%d-%T)_grim.png"
+		SELECTION="slurp | grim -g - ~/$FILENAME"
+		WHOLESCREEN="grim ~/$FILENAME"
+		;;
+	xorg|dwm)
+		SELECTION="scrot -e 'echo $f | xsel -i -b' -d 1 -s -q 1"
+		WHOLESCREEN="scrot -e 'echo $f | xsel -i -b' -d 1 -q 1"
+		;;
+	ssh)
+		echo "cannot screenshot ssh ;)"
+		exit 0
+		;;
+esac
+
 if [ "$1" = "selection" ]; then
-	notify-send "select an area" && scrot -e 'echo $f | xsel -i -b' -d 1 -s -q 1 && notify-send "screenshot saved, filename copied to clipboard"
+	notify-send "select an area" && eval "$SELECTION" && notify-send "screenshot saved, filename copied to clipboard"
 else
-	scrot -e 'echo $f | xsel -i -b' -d 1 -q 1 && notify-send "screenshot saved, filename copied to clipboard"
+	eval "$WHOLESCREEN" && notify-send "screenshot saved, filename copied to clipboard"
 fi
