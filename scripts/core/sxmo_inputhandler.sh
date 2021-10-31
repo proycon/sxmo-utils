00ade0167ed9779f1a8e3725abb913a3cb995285
Maarten van Gompel <proycon@anaproy.nl>
2021-10-31 13:32:44 +0100

    limit inputhandler behaviour in locked state
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/core/sxmo_inputhandler.sh
+++ scripts/core/sxmo_inputhandler.sh
@@ -36,6 +36,19 @@ XPROPOUT="$(sxmo_wm.sh focusedwindow)"
 WMCLASS="$(printf %s "$XPROPOUT" | grep app: | cut -d" " -f2- | tr '[:upper:]' '[:lower:]')"
 WMNAME="$(printf %s "$XPROPOUT" | grep title: | cut -d" " -f2- | tr '[:upper:]' '[:lower:]')"
 
+if [ "$(sxmo_screenlock.sh getCurState)" != "unlock" ]; then
+	case "$ACTION" in
+		"powerbutton_one")
+			lock_screen_action
+			;;
+		"powerbutton_two"|"powerbutton_three")
+			lock_screen_action 2
+			;;
+	esac
+	#we're locked, don't process the rest of the script
+	exit 0
+fi
+
 if [ -x "$XDG_CONFIG_HOME"/sxmo/hooks/inputhandler ]; then
 	#hook script must exit with a zero exit code ONLY if it has handled the gesture!
 	"$XDG_CONFIG_HOME"/sxmo/hooks/inputhandler "$WMCLASS" "$WMNAME" "$@" && exit
