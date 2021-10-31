a9fe5d139b4aea3221f868539cd7ec0157cca362
Stacy Harper <contact@stacyharper.net>
2021-10-31 16:29:19 +0100

    Detect as valid multiple composed LOGDIR

--- scripts/core/sxmo_validnumber.sh
+++ scripts/core/sxmo_validnumber.sh
@@ -3,9 +3,18 @@
 # shellcheck source=scripts/core/sxmo_common.sh
 . "$(dirname "$0")/sxmo_common.sh"
 
-if pn valid "$1"; then
+returnvalid() {
 	printf %s "$1"
 	exit
+}
+
+if [ "$(printf "%b\n" "$1" | xargs -0 pn find | xargs printf %s)" = "$1" ]; then
+	# a multiple formated phone number
+	returnvalid "$1"
+fi
+
+if pn valid "$1"; then
+	returnvalid "$1"
 fi
 
 REFORMATTED="$(pn find ${DEFAULT_COUNTRY:+-c "$DEFAULT_COUNTRY"} "$1")"
@@ -18,8 +27,7 @@ notify-send "\"$1\" is not a valid phone number"
 
 PICKED="$(printf "Ok\nUse as it is\n" | sxmo_dmenu.sh -p "Invalid Number")"
 if [ "$PICKED" = "Use as it is" ]; then
-	printf %s "$1"
-	exit
+	returnvalid "$1"
 fi
 
 exit 1
