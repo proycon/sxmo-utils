f8bf899d4f6f90e038a1b1b74e31f98064f7c381
ArenM <aren@peacevolution.org>
2021-11-28 18:44:09 -0500

    Add support for Arch Linux to sxmo_upgrade.sh
    Signed-off-by: Maarten van Gompel <proycon@anaproy.nl>

--- scripts/core/sxmo_upgrade.sh
+++ scripts/core/sxmo_upgrade.sh
@@ -3,11 +3,26 @@
 # shellcheck source=scripts/core/sxmo_common.sh
 . "$(dirname "$0")/sxmo_common.sh"
 
-echo "Updating all packages from repositories"
-doas apk update
+trap "read -r" EXIT
 
-echo "Upgrading all packages"
-doas apk upgrade
+update_apk() {
+	echo "Updating all packages from repositories"
+	doas apk update
 
-echo "Upgrade complete - reboot for all changes to take effect"
-read -r
+	echo "Upgrading all packages"
+	doas apk upgrade
+
+	echo "Upgrade complete - reboot for all changes to take effect"
+}
+
+update_pacman() {
+	echo "Upgrading all packages"
+	doas pacman -Syu
+
+	echo "Upgrade complete - reboot for all changes to take effect"
+}
+
+case "$OS" in
+	"Alpine Linux"|postmarketOS) upgrade_apk;;
+	"Arch Linux ARM"|alarm) upgrade_pacman;;
+esac
