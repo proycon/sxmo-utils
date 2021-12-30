dc223bd56b1dea439a92e3e19e83094956c73424
Clayton Craft <clayton@craftyguy.net>
2021-12-30 12:57:04 -0800

    core: simplify setled, support setting type in device profile
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/core/sxmo_setled.sh
+++ scripts/core/sxmo_setled.sh
@@ -7,32 +7,25 @@ usage () {
 
 [ $# -lt 2 ] && usage
 
-case "$(cut -d, -f2 < /sys/firmware/devicetree/base/compatible)" in
-	l8910qcom) # bq-paella
-		color="white"; type="kbd_backlight";;
-	pinephone-prorockchip) # pinephone pro
-		case $1 in
-			red) color="$1"; type="standby" ;;
-			green) color="$1"; type="disk-activity" ;;
-			blue) color="$1"; type="charging" ;;
-			white) color="$1"; type="flash" ;; #TODO: Not found on pinephone pro yet!
-			*) usage ;;
-		esac
-		;;
-	*) # assume pinephone/pinetab
-		case $1 in
-			red|green|blue) color="$1"; type="indicator" ;;
-			white) color="$1"; type="flash" ;;
-			*) usage ;;
-		esac
-		;;
-esac
-
 if [ "$2" -lt 0 ] || [ "$2" -gt 255 ]
 then
 	usage
 fi
 
+color="$1"
 brightness="$2"
+
+# Get type from variable name created dynamically from the color,
+# e.g. $LED_RED_TYPE
+eval type='$'LED_"$(echo "$color" | tr '[:lower:]' '[:upper:]')"_TYPE
+
+# Defaults
+if [ -z "$type" ]; then
+	case $color in
+		red|green|blue) color="$1"; type="indicator" ;;
+		white) color="$1"; type="flash" ;;
+		*) usage ;;
+	esac
+fi
 
 printf "%s\n" "$brightness" > "/sys/class/leds/$color:$type/brightness"
--- scripts/deviceprofiles/sxmo_deviceprofile_l8150qcom.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_l8150qcom.sh
@@ -6,3 +6,4 @@ export TOUCH_POINTER_ID="10"
 
 export ROTATION_GRAVITY="500"
 export ROTATION_THRESHOLD="60"
+export LED_WHITE_TYPE="kbd_backlight"
