707b82bef9ca3dedbb3fd0ac3b3813327fbf2967
Clayton Craft <clayton@craftyguy.net>
2021-12-30 12:57:05 -0800

    core: allow specifying flash LED path in device profile
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/core/sxmo_flashtoggle.sh
+++ scripts/core/sxmo_flashtoggle.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+[ -z "$FLASH_LED" ] && FLASH_LED=/sys/class/leds/white:flash
+
 sxmo_setled.sh white "$(
-	grep -qE '^0$' /sys/class/leds/white:flash/brightness &&
+	grep -qE '^0$' "$FLASH_LED"/brightness &&
 	echo 255 || echo 0
 )"
 
