4bdf919dfdde41334a069e889b54bf0d4fd51edc
Peter John Hartman <peterjohnhartman@gmail.com>
2022-02-02 10:48:38 -0600

    sxmo_setpermissions.sh: use info from deviceprofile
    This way if you add a new device you only need to modify the
    deviceprofile file to establish all the device specific things.
    
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/core/sxmo_setpermissions.sh
+++ scripts/core/sxmo_setpermissions.sh
@@ -4,24 +4,14 @@
 # from the init process and sets
 # some device-specific permissions
 
-DEVICE="unknown"
+device="$(cut -d ',' -f 2 < /sys/firmware/devicetree/base/compatible | tr -d '\0')"
+deviceprofile="$(which "sxmo_deviceprofile_$device.sh")"
+# shellcheck disable=SC1090
+[ -f "$deviceprofile" ] && . "$deviceprofile"
 
-#Detecting device
-if [ -e /sys/firmware/devicetree/base ]; then
-    if grep -q pinephone /sys/firmware/devicetree/base/compatible; then
-        DEVICE="pinephone"
-    fi
-fi
-
-pinephone_files="/sys/module/8723cs/parameters/rtw_scan_interval_thr /sys/power/state /sys/devices/platform/soc/1f00000.rtc/power/wakeup /sys/power/mem_sleep /sys/bus/usb/drivers/usb/unbind /sys/bus/usb/drivers/usb/bind /dev/rtc0 /sys/devices/platform/soc/1f03400.rsb/sunxi-rsb-3a3/axp221-pek/power/wakeup"
-
-if [ "$DEVICE" = "pinephone" ]; then
-    files="$pinephone_files"
-else
-    #guess a few that are hopefully fairly generic:
-    files="/sys/power/state /sys/power/mem_sleep /sys/bus/usb/drivers/usb/unbind /sys/bus/usb/drivers/usb/bind /dev/rtc0"
-    echo "Warning: SXMO is running on an unknown device, things may not work as expected!">&2
-fi
+# the defaults are pinephone
+# users can override this in sxmo_deviceprofile_mydevice.sh
+files="${SXMO_SYS_FILES:-"/sys/module/8723cs/parameters/rtw_scan_interval_thr /sys/power/state /sys/devices/platform/soc/1f00000.rtc/power/wakeup /sys/power/mem_sleep /sys/bus/usb/drivers/usb/unbind /sys/bus/usb/drivers/usb/bind /dev/rtc0 /sys/devices/platform/soc/1f03400.rsb/sunxi-rsb-3a3/axp221-pek/power/wakeup"}"
 
 for file in $files; do
     [ -e "$file" ] && chmod a+rw "$file"
