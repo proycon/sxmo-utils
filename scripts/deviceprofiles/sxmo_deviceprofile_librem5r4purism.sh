94ca53d395806ed1a6fa9da4f52f4ef6d844cc17
Peter John Hartman <peterjohnhartman@gmail.com>
2022-02-02 10:48:41 -0600

    Removed unused BACKLIGHT.
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/deviceprofiles/sxmo_deviceprofile_librem5r4purism.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_librem5r4purism.sh
@@ -3,7 +3,6 @@
 export SXMO_SPEAKER="Speaker"
 export SXMO_HEADPHONE="Headphone"
 export SXMO_EARPIECE="Earpiece"
-export BACKLIGHT="/sys/devices/platform/backlight-dsi/backlight/backlight-dsi"
 export LED_RED_TYPE="status"
 export LED_GREEN_TYPE="status"
 export LED_BLUE_TYPE="status"
