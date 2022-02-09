f372b841ca5a4e1a4b031f02eb80e92117d46db9
Stacy Harper <contact@stacyharper.net>
2022-02-09 18:37:04 +0100

    fix some configversion comment type

--- setup_config_version.sh
+++ setup_config_version.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+comment="#"
+case "$(head -n1 "$1")" in
+	!*)
+		comment="!"
+		;;
+	--*)
+		comment="--"
+		;;
+esac
+
 busybox md5sum "$1" | \
 	cut -d" " -f1 | \
-	xargs -I{} sed -i '2i# configversion: {}' \
+	xargs -I{} sed -i "2i$comment configversion: {}" \
 	"$1"
