f4b3fedc5ce3d5ed111a95a9d0bff910feb5dd42
Stacy Harper <contact@stacyharper.net>
2022-02-11 09:45:47 +0100

    Make setpermissions files device specific

--- scripts/core/sxmo_setpermissions.sh
+++ scripts/core/sxmo_setpermissions.sh
@@ -4,14 +4,16 @@
 # from the init process and sets
 # some device-specific permissions
 
-device="$(cut -d ',' -f 2 < /sys/firmware/devicetree/base/compatible | tr -d '\0')"
-deviceprofile="$(which "sxmo_deviceprofile_$device.sh")"
-# shellcheck disable=SC1090
-[ -f "$deviceprofile" ] && . "$deviceprofile"
+if [ -e /sys/firmware/devicetree/base/compatible ]; then
+	device="$(cut -d ',' -f 2 < /sys/firmware/devicetree/base/compatible | tr -d '\0')"
+	deviceprofile="$(which "sxmo_deviceprofile_$device.sh")"
+	# shellcheck disable=SC1090
+	[ -f "$deviceprofile" ] && . "$deviceprofile"
+fi
 
-# the defaults are pinephone
+# the defaults are best guesses
 # users can override this in sxmo_deviceprofile_mydevice.sh
-files="${SXMO_SYS_FILES:-"/sys/module/8723cs/parameters/rtw_scan_interval_thr /sys/power/state /sys/devices/platform/soc/1f00000.rtc/power/wakeup /sys/power/mem_sleep /sys/bus/usb/drivers/usb/unbind /sys/bus/usb/drivers/usb/bind /dev/rtc0 /sys/devices/platform/soc/1f03400.rsb/sunxi-rsb-3a3/axp221-pek/power/wakeup"}"
+files="${SXMO_SYS_FILES:-"/sys/power/state /sys/power/mem_sleep /sys/bus/usb/drivers/usb/unbind /sys/bus/usb/drivers/usb/bind /dev/rtc0"}"
 
 for file in $files; do
     [ -e "$file" ] && chmod a+rw "$file"
