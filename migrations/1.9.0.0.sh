30c6ccb5eace643658888df30a124b2fe2c7b94a
Stacy Harper <contact@stacyharper.net>
2022-02-12 10:21:58 +0100

    Dont do anything if there is no hooks at all

--- migrations/1.9.0.0.sh
+++ migrations/1.9.0.0.sh
@@ -3,8 +3,8 @@
 # 1.9.0 introduced a new naming scheme for hooks.
 # This script moves them from their 1.8.x location.
 
-mkdir -p "$XDG_CONFIG_HOME/sxmo/hooks/$SXMO_DEVICE_NAME"
 cd "$XDG_CONFIG_HOME/sxmo/hooks/" || exit
+mkdir -p "$XDG_CONFIG_HOME/sxmo/hooks/$SXMO_DEVICE_NAME"
 
 [ -e inputhandler ] && mv inputhandler "$SXMO_DEVICE_NAME/sxmo_hook_inputhandler.sh"
 [ -e lock ] && mv lock "$SXMO_DEVICE_NAME/sxmo_hook_lock.sh"
