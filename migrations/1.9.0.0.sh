b031bd552509eb78a8308e688b1ec29ea4c21a21
Stacy Harper <contact@stacyharper.net>
2022-02-12 10:15:27 +0100

    Migration 1.9.0, off hook has been renamed to screenoff

--- migrations/1.9.0.0.sh
+++ migrations/1.9.0.0.sh
@@ -8,7 +8,7 @@ cd "$XDG_CONFIG_HOME/sxmo/hooks/" || exit
 
 [ -e inputhandler ] && mv inputhandler "$SXMO_DEVICE_NAME/sxmo_hook_inputhandler.sh"
 [ -e lock ] && mv lock "$SXMO_DEVICE_NAME/sxmo_hook_lock.sh"
-[ -e off ] && mv off "$SXMO_DEVICE_NAME/sxmo_hook_off.sh"
+[ -e off ] && mv off "$SXMO_DEVICE_NAME/sxmo_hook_screenoff.sh"
 [ -e unlock ] && mv unlock "$SXMO_DEVICE_NAME/sxmo_hook_unlock.sh"
 
 find . -type f -maxdepth 1 -exec basename {} \; | grep -v '^sxmo_hook_.*\.sh$' | xargs -I{} mv {} sxmo_hook_{}.sh
