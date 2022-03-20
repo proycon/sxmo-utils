proycon/sxmo-utils

commit c3991d4b9ae6136e887a47cd09e7476744ae0349

Allow user to override SXMO_ vars in .profile
This allows for that possibility.  I'm not sure *when* it'd be useful to
override in .profile vs. ~/.config/sxmo/profile, but a user brought it
up on IRC, and this makes it consistent with all the other variables.

Signed-off-by: Stacy Harper <contact@stacyharper.net>

configs/profile.d/sxmo_init.sh
@@ -37,21 +37,22 @@ _sxmo_load_environments() {
 	fi
 	export OS="${ID:-unknown}"
 
-	export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-	export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-	export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+	export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME}/.local/share"
+	export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME}/.cache"
+	export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME}/.config"
+	export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME}/.local/state"
 	export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/dev/shm/user/$(id -u)}"
 
-	export SXMO_CACHEDIR="$XDG_CACHE_HOME"/sxmo
-	export SXMO_DEBUGLOG="${XDG_STATE_HOME:-$HOME/.local/state}/tinydm.log"
+	export SXMO_CACHEDIR="${SXMO_CACHEDIR:-$XDG_CACHE_HOME}/sxmo"
+	export SXMO_DEBUGLOG="${SXMO_DEBUGLOG:-$XDG_STATE_HOME}/tinydm.log"
 
-	export SXMO_BLOCKDIR="$XDG_DATA_HOME/sxmo/block"
-	export SXMO_BLOCKFILE="$XDG_CONFIG_HOME/sxmo/block.tsv"
-	export SXMO_CONTACTFILE="$XDG_CONFIG_HOME/sxmo/contacts.tsv"
-	export SXMO_STATE="$XDG_RUNTIME_DIR/sxmo.state"
-	export SXMO_LOGDIR="$XDG_DATA_HOME/sxmo/modem"
-	export SXMO_NOTIFDIR="$XDG_DATA_HOME/sxmo/notifications"
-	export SXMO_UNSUSPENDREASONFILE="$XDG_RUNTIME_DIR/sxmo.suspend.reason"
+	export SXMO_BLOCKDIR="${SXMO_BLOCKDIR:-$SXMO_CACHEDIR}/block"
+	export SXMO_BLOCKFILE="${SXMO_BLOCKFILE:-$SXMO_CACHEDIR}/block.tsv"
+	export SXMO_CONTACTFILE="${SXMO_CONTACTFILE:-$SXMO_CACHEDIR}/contacts.tsv"
+	export SXMO_STATE="${SXMO_STATE:-$XDG_RUNTIME_DIR}/sxmo.state"
+	export SXMO_LOGDIR="${SXMO_LOGDIR:-$XDG_DATA_HOME}/sxmo/modem"
+	export SXMO_NOTIFDIR="${SXMO_NOTIFDIR:-$SXMO_CACHEDIR}/notifications"
+	export SXMO_UNSUSPENDREASONFILE="${SXMO_UNSUSPENDREASONFILE:-$XDG_RUNTIME_DIR}/sxmo.suspend.reason"
 
 	export BEMENU_OPTS="${BEMENU_OPTS:---fn 'Monospace 14' --scrollbar autohide -s -n -w -c -l8 -M 40 -H 20}"
 

commit 3fa74106e64b825fb17a9eb88f5a2a292b5681e4

Fix SXMO_ vars in profile.d
After updating, sxmo_migrate.sh in a ssh session was broken, with
a strange error:

    usr/share/sxmo/migrations/1.9.0.0.sh: cd: line 8: can't cd to /home/user/.config/.config/.config/sxmo/hooks/: No such file or directory

This patch fix the issue introduced by c3991d4.

I'm not sure why phartman changed this in a second second version of
the patch, it was correct in the first one. Maybe I'm missing something.

Signed-off-by: Peter John Hartman <peterjohnhartman@gmail.com>

configs/profile.d/sxmo_init.sh
@@ -39,22 +39,22 @@ _sxmo_load_environments() {
 	fi
 	export OS="${ID:-unknown}"
 
-	export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME}/.local/share"
-	export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME}/.cache"
-	export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME}/.config"
-	export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME}/.local/state"
+	export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+	export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+	export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+	export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 	export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/dev/shm/user/$(id -u)}"
 
-	export SXMO_CACHEDIR="${SXMO_CACHEDIR:-$XDG_CACHE_HOME}/sxmo"
-	export SXMO_DEBUGLOG="${SXMO_DEBUGLOG:-$XDG_STATE_HOME}/tinydm.log"
+	export SXMO_CACHEDIR="${SXMO_CACHEDIR:-$XDG_CACHE_HOME/sxmo}"
+	export SXMO_DEBUGLOG="${SXMO_DEBUGLOG:-$XDG_STATE_HOME/tinydm.log}"
 
-	export SXMO_BLOCKDIR="${SXMO_BLOCKDIR:-$SXMO_CACHEDIR}/block"
-	export SXMO_BLOCKFILE="${SXMO_BLOCKFILE:-$SXMO_CACHEDIR}/block.tsv"
-	export SXMO_CONTACTFILE="${SXMO_CONTACTFILE:-$SXMO_CACHEDIR}/contacts.tsv"
-	export SXMO_STATE="${SXMO_STATE:-$XDG_RUNTIME_DIR}/sxmo.state"
-	export SXMO_LOGDIR="${SXMO_LOGDIR:-$XDG_DATA_HOME}/sxmo/modem"
-	export SXMO_NOTIFDIR="${SXMO_NOTIFDIR:-$SXMO_CACHEDIR}/notifications"
-	export SXMO_UNSUSPENDREASONFILE="${SXMO_UNSUSPENDREASONFILE:-$XDG_RUNTIME_DIR}/sxmo.suspend.reason"
+	export SXMO_BLOCKDIR="${SXMO_BLOCKDIR:-$SXMO_CACHEDIR/block}"
+	export SXMO_BLOCKFILE="${SXMO_BLOCKFILE:-$SXMO_CACHEDIR/block.tsv}"
+	export SXMO_CONTACTFILE="${SXMO_CONTACTFILE:-$SXMO_CACHEDIR/contacts.tsv}"
+	export SXMO_STATE="${SXMO_STATE:-$XDG_RUNTIME_DIR/sxmo.state}"
+	export SXMO_LOGDIR="${SXMO_LOGDIR:-$XDG_DATA_HOME/sxmo/modem}"
+	export SXMO_NOTIFDIR="${SXMO_NOTIFDIR:-$SXMO_CACHEDIR/notifications}"
+	export SXMO_UNSUSPENDREASONFILE="${SXMO_UNSUSPENDREASONFILE:-$XDG_RUNTIME_DIR/sxmo.suspend.reason}"
 
 	export BEMENU_OPTS="${BEMENU_OPTS:---fn 'Monospace 14' --scrollbar autohide -s -n -w -c -l8 -M 40 -H 20}"
 