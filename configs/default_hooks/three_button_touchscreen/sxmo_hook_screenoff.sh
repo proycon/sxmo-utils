c82742ba9c89357604d6e04ce327df80025ab941
Stacy Harper <contact@stacyharper.net>
2022-03-31 17:48:19 +0000

    Prevent missing modem notification on slower devices
    On pinephone pro we loose the modem status on wake ups. It means modem
    manager have to reset its modem status to actuall fetch sms or calls.
    It generally take longer than 5 seconds so sxmo go back to crust before.
    
    30 is lazy enough to cover the case where the modem is broken and we
    dont want to wait indefinitely.

--- configs/default_hooks/three_button_touchscreen/sxmo_hook_screenoff.sh
+++ configs/default_hooks/three_button_touchscreen/sxmo_hook_screenoff.sh
@@ -36,7 +36,7 @@ esac
 # Start a periodic daemon (2s) blink after 5 seconds
 # Resume tasks stop daemons
 sxmo_daemons.sh start idle_locker sxmo_idle.sh -w \
-	timeout 8 'sxmo_daemons.sh start going_deeper sxmo_run_periodically.sh 5 sh -c "sxmo_hook_check_state_mutexes.sh && exec sxmo_mutex.sh can_suspend holdexec sxmo_suspend.sh"' \
+	timeout 8 'sxmo_daemons.sh start going_deeper sxmo_run_periodically.sh 30 sh -c "sxmo_hook_check_state_mutexes.sh && exec sxmo_mutex.sh can_suspend holdexec sxmo_suspend.sh"' \
 	resume 'sxmo_daemons.sh stop going_deeper' \
 	timeout 5 'sxmo_daemons.sh start periodic_blink sxmo_run_periodically.sh 2 sxmo_uniq_exec.sh sxmo_led.sh blink red blue' \
 	resume 'sxmo_daemons.sh stop periodic_blink' \
