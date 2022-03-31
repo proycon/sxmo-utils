58549b7e35c4405a72e479bce99b16d625441ae2
mungfusensei <mungfusensei@gmail.com>
2022-03-31 15:58:44 -0400

    Fix mem_sleep typo in pinephonepro device profile
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/deviceprofiles/sxmo_deviceprofile_pinephone-prorockchip.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_pinephone-prorockchip.sh
@@ -2,8 +2,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright 2022 Sxmo Contributors
 
-export SXMO_SYS_FILES="/sys/module/8723cs/parameters/rtw_scan_interval_thr /sys/power/state /sys/devices/platform/soc/1f00000.rtc/power/wakeup /sys/power/mem_s
-eep /sys/bus/usb/drivers/usb/unbind /sys/bus/usb/drivers/usb/bind /dev/rtc0 /sys/devices/platform/soc/1f03400.rsb/sunxi-rsb-3a3/axp221-pek/power/wakeup"
+export SXMO_SYS_FILES="/sys/module/8723cs/parameters/rtw_scan_interval_thr /sys/power/state /sys/devices/platform/soc/1f00000.rtc/power/wakeup /sys/power/mem_sleep /sys/bus/usb/drivers/usb/unbind /sys/bus/usb/drivers/usb/bind /dev/rtc0 /sys/devices/platform/soc/1f03400.rsb/sunxi-rsb-3a3/axp221-pek/power/wakeup"
 export SXMO_TOUCHSCREEN_ID=8
 export SXMO_POWER_BUTTON="1:1:gpio-key-power"
 export SXMO_VOLUME_BUTTON="1:1:adc-keys"
