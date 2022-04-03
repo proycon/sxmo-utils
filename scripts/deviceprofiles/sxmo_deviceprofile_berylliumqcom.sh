44ea8c8b27ad49eec420bf94e4090e792d71ddc3
Anjandev Momi <anjan@momi.ca>
2022-04-03 15:52:37 -0400

    add poco f1 (codename beryllium) initial support
    This patch makes most things work again on the poco f1. However, volume
    up key is broken - this is a regression. Volume up used to work on 1.8.2.
    
    I dont know how to debug or fix this. I have a poco f1 and can provide
    logs.
    
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_berylliumqcom.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# poco f1
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+export SXMO_DISABLE_LEDS="1"
+export SXMO_MONITOR="0:0:Novatek_NT36XXX_Touchscreen"
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_VOLUME_BUTTON="0:0:pm8941_resin"
