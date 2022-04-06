ae37a07319ca302605a85215e0a5d6538081c7f3
Stacy Harper <contact@stacyharper.net>
2022-04-06 16:24:13 +0200

    Avoid the dangling periodic blink

--- configs/default_hooks/three_button_touchscreen/sxmo_hook_lock.sh
+++ configs/default_hooks/three_button_touchscreen/sxmo_hook_lock.sh
@@ -19,6 +19,9 @@ sxmo_wm.sh dpms off
 sxmo_wm.sh inputevent touchscreen off
 sxmo_daemons.sh stop lisgd
 
+# avoid dangling purple blinking when usb wakeup + power button…
+sxmo_daemons.sh stop periodic_blink
+
 wait "$LEDPID"
 
 # Go to screenoff after 8 seconds of inactivity
