71219137200a81546175c99f097497772bff1bda
Peter John Hartman <peterjohnhartman@gmail.com>
2022-04-07 19:35:15 -0500

    Add bq paella and oneplus enchilada deviceprofiles
    A first stab based only on info given me in input_outputs.
    
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_enchiladaqcom.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+# TODO: TESTME!
+export SXMO_VOLUME_BUTTON="1:1:Volume_keys"
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
--- scripts/deviceprofiles/sxmo_deviceprofile_longcheer,l8150.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_longcheer,l8150.sh
@@ -4,7 +4,8 @@
 # Wileyfox Swift (wileyfox-crackling)
 
 export SXMO_TOUCHSCREEN_ID="10"
-
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_VOLUME_BUTTON="1:1:GPIO_Buttons 0:0:pm8941_resin"
 export SXMO_ROTATION_GRAVITY="500"
 export SXMO_ROTATION_THRESHOLD="60"
 export LED_WHITE_TYPE="kbd_backlight"
