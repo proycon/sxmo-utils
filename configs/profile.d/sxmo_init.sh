6f549dec1428fb76fd5859c2a05b705f84964987
Stacy Harper <contact@stacyharper.net>
2022-04-13 15:25:24 +0200

    Remove useless variable

--- configs/profile.d/sxmo_init.sh
+++ configs/profile.d/sxmo_init.sh
@@ -42,7 +42,6 @@ _sxmo_load_environments() {
 	export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 	export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 	export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-	export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 	export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/dev/shm/user/$(id -u)}"
 
 	export SXMO_CACHEDIR="${SXMO_CACHEDIR:-$XDG_CACHE_HOME/sxmo}"
