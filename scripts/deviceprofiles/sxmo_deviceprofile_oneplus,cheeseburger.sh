bbaa242f01c3764ff7659c986fdc8c416c9bf7c6
Jami Kettunen <jamipkettunen@gmail.com>
2022-04-14 22:05:24 +0300

    Add initial OnePlus 5/5T support
    Changes in v2:
    - correct LED_WHITE_TYPE var to SXMO_LED_WHITE_TYPE for cheeseburger
    
    Signed-off-by: Jami Kettunen <jami.kettunen@protonmail.com>
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_oneplus,cheeseburger.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+# OnePlus 5
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_VOLUME_BUTTON="1:1:Volume_buttons"
+export SXMO_LED_WHITE_TYPE="kbd_backlight"
+#export SXMO_TOUCHSCREEN_ID="" # TODO for dwm/xorg support.
---
+++ scripts/deviceprofiles/sxmo_deviceprofile_oneplus,dumpling.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+# OnePlus 5T
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_VOLUME_BUTTON="1:1:Volume_buttons"
+export SXMO_DISABLE_LEDS="1" # until support for Light Pulse Generator (LPG) is implemented in sxmo_led.sh
+#export SXMO_TOUCHSCREEN_ID="" # TODO for dwm/xorg support.
