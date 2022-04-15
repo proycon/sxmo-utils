e865ea7b2381cd621827f04613aa9ae6bdf583bb
Jami Kettunen <jamipkettunen@gmail.com>
2022-04-16 02:54:49 +0300

    deviceprofiles: oneplus,cheeseburger: Disable all LEDs for now
    There's constant spam about red/blue/green LEDs in logs which simply
    cannot be controlled yet, and the white keypad backlight LED only seems
    to be able to get controlled by the flashlight toggle in system menu
    anyway.
    
    Signed-off-by: Jami Kettunen <jami.kettunen@protonmail.com>
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

--- scripts/deviceprofiles/sxmo_deviceprofile_oneplus,cheeseburger.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_oneplus,cheeseburger.sh
@@ -5,5 +5,5 @@
 # OnePlus 5
 export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
 export SXMO_VOLUME_BUTTON="1:1:Volume_buttons"
-export SXMO_LED_WHITE_TYPE="kbd_backlight"
+export SXMO_DISABLE_LEDS="1" # until support for Light Pulse Generator (LPG) is implemented in sxmo_led.sh
 #export SXMO_TOUCHSCREEN_ID="" # TODO for dwm/xorg support.
