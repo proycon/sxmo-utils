b005014c9aa0828b0cf96ce1626a0b1667a0e3d9
Jami Kettunen <jamipkettunen@gmail.com>
2022-04-15 20:54:44 +0300

    migrations: 1.9.0.0: silence error on cd to hooks dir
    Silences following from logs (in case user has no custom hooks):
    
      /usr/share/sxmo/migrations/1.9.0.0.sh: 8: cd: can't cd to /home/<user>/.config/sxmo/hooks/
    
    Since the migration script exits anyway if this cd fails this just
    pollutes logs during startup.
    
    Signed-off-by: Jami Kettunen <jami.kettunen@protonmail.com>
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

--- migrations/1.9.0.0.sh
+++ migrations/1.9.0.0.sh
@@ -5,7 +5,7 @@
 # 1.9.0 introduced a new naming scheme for hooks.
 # This script moves them from their 1.8.x location.
 
-cd "$XDG_CONFIG_HOME/sxmo/hooks/" || exit
+cd "$XDG_CONFIG_HOME/sxmo/hooks/" 2>/dev/null || exit
 mkdir -p "$XDG_CONFIG_HOME/sxmo/hooks/$SXMO_DEVICE_NAME"
 
 [ -e inputhandler ] && mv inputhandler "$SXMO_DEVICE_NAME/sxmo_hook_inputhandler.sh"
