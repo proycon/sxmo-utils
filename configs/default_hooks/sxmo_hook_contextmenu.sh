0c15d9056685c9fab615ca45ed77c108dec3124a
ArenM <aren@peacevolution.org>
2022-04-20 00:25:23 -0400

    prefer swaymsg exit and sigterm over sigkill to logout
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- configs/default_hooks/sxmo_hook_contextmenu.sh
+++ configs/default_hooks/sxmo_hook_contextmenu.sh
@@ -116,7 +116,7 @@ case "$WMCLASS" in
 			$icon_lck Lock               ^ 0 ^ sxmo_hook_lock.sh
 			$icon_lck Lock (Screen off)  ^ 0 ^ sxmo_hook_screenoff.sh
 			$icon_zzz Suspend            ^ 0 ^ sxmo_hook_screenoff.sh && sxmo_suspend.sh
-			$icon_out Logout             ^ 0 ^ confirm Logout && (pkill -9 dwm || pkill -9 sway)
+			$icon_out Logout             ^ 0 ^ confirm Logout && (pkill dwm || swaymsg exit)
 			$icon_rol Toggle WM          ^ 0 ^ confirm Toggle && sxmo_wmtoggle.sh
 			$icon_rld Reboot             ^ 0 ^ confirm Reboot && sxmo_power.sh reboot
 			$icon_pwr Poweroff           ^ 0 ^ confirm Poweroff && sxmo_power.sh poweroff
