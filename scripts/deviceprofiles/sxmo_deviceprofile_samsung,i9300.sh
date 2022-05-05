dc657a8c73c6ffadb651b88c0173d881323a6b82
Newbyte <newbyte@disroot.org>
2022-05-05 16:53:43 +0200

    Add initial Samsung Galaxy S III support
    I see I messed up the first one by not including where it was to go.
    This should be better.
    
    Signed-off-by: Newbyte <newbyte@disroot.org>
    Signed-off-by: Peter John Hartman <peterjohnhartman@gmail.com>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_samsung,i9300.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Newbyte <newbyte@disroot.org>
+
+# Samsung Galaxy S III
+export SXMO_POWER_BUTTON="0:0:max77693-muic/dock"
+export SXMO_VOLUME_BUTTON="1:1:gpio-keys"
