d987a2b9a3444c92e96fb53b9c3fb7acf7c7d30a
Stacy Harper <contact@stacyharper.net>
2022-05-12 10:58:09 +0200

    Avoid dangling lisgd process if it crash
    superd doesnt manage the execed subprocess. It means if lisgd crash
    somehow, superd will continue to see it as running.
    
    Conversation :
    
    https://todo.sr.ht/~craftyguy/superd/30
    
    Not using exec seems more reliable for now in the superd context.

--- configs/default_hooks/sxmo_hook_lisgdstart.sh
+++ configs/default_hooks/sxmo_hook_lisgdstart.sh
@@ -22,7 +22,7 @@ fi
 #-g format:
 #   fingers,swipe,edge,distance,command
 #order matters, only the first match gets executed
-exec lisgd "$@" -d "$LISGD_INPUT_DEVICE" ${orientation:+-o $orientation} \
+lisgd "$@" -d "$LISGD_INPUT_DEVICE" ${orientation:+-o $orientation} \
 	-t "$LISGD_THRESHOLD" -T "$LISGD_THRESHOLD_PRESSED" \
 	-g '1,DRUL,BR,*,setsid -f sxmo_hook_inputhandler.sh bottomrightcorner' \
 	-g '1,DLUR,BL,*,setsid -f sxmo_hook_inputhandler.sh bottomleftcorner' \
