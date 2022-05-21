351688d03844fa904d1aa2c2c7ff12d11e9890bd
xengineering <me@xengineering.eu>
2022-05-21 18:41:46 +0200

    Add initial support for samsung-gt510
    Hi all,
    
    I just tested SWMO on my new samsung-gt510 [1]. SWMO is displayed but input via
    buttons and touchscreen does not work.
    
    With this patch the device is quite usable. Some functionality (like LEDs and
    modem) is missing / not tested, but this could already be 80 % of the port.
    
    Greetings and thank you for your great work
    xengineering
    
    [1] https://wiki.postmarketos.org/wiki/Samsung_Galaxy_Tab_A_9.7_2015_(samsung-gt510)
    
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_samsung,gt510.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# author: xengineering <me@xengineering.eu>
+
+# Samsung Galaxy Tab A 9.7 2015 (tested on LTE variant, Code SM-T555)
+export SXMO_DISABLE_LEDS="1"
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_VOLUME_BUTTON="1:1:GPIO_Buttons 0:0:pm8941_resin"
