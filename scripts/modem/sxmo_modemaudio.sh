9f5346f042bf232b3914a58e04f87b4ddb2e9d69
ArenM <aren@peacevolution.org>
2022-06-30 06:40:08 -0500

    fix conflicting audio settings (#502)
    I agree with the spirit, but I think the retry loops are needed. I added
    some log output so we can track it for now.  I still wasn't sure about
    the moving of unmute mic from beginning to end, so I wanted to send this
    to the list before I apply it.
    
    (Original Comment:
    When callaudiod disables call audio mode it always enables the mic.
    Because we were disabling the mic before disabling call audio mode
    reset_audio always fails.
    
    I also removed the retry loop; if the software we rely on breaks we
    should fix it upstream, rather than hitting it with a hammer until it
    works.
    
    This should fix https://todo.sr.ht/~mil/sxmo-tickets/502
    End Original Comment)
    
    Signed-off-by: Peter John Hartman <peterjohnhartman@gmail.com>
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/modem/sxmo_modemaudio.sh
+++ scripts/modem/sxmo_modemaudio.sh
@@ -4,9 +4,9 @@
 . sxmo_common.sh
 
 setup_audio() {
-	unmute_mic
 	enable_call_audio_mode
 	disable_speaker
+	unmute_mic
 
 	i=0
 	while (! is_unmuted_mic) || (! is_call_audio_mode) || (! is_disabled_speaker); do
@@ -17,17 +17,18 @@ setup_audio() {
 
 		sleep 0.2
 
-		unmute_mic
 		enable_call_audio_mode
 		disable_speaker
+		umute_mic
 	done
 }
 
 reset_audio() {
-	mute_mic
 	disable_call_audio_mode
 	enable_speaker
+	mute_mic
 
+	# eventually we should remove this loop once upstream is stable
 	i=0
 	while (! is_muted_mic) || (! is_default_audio_mode) || (! is_enabled_speaker); do
 		i=$((i+1))
@@ -37,9 +38,9 @@ reset_audio() {
 
 		sleep 0.2
 
-		mute_mic
 		disable_call_audio_mode
 		enable_speaker
+		mute_mic
 	done
 }
 
@@ -52,11 +53,11 @@ is_unmuted_mic() {
 }
 
 mute_mic() {
-	callaudiocli -u 1
+	callaudiocli -u 1 || sxmo_log "ERR: callaudiocli -u 1 failed"
 }
 
 unmute_mic() {
-	callaudiocli -u 0
+	callaudiocli -u 0 || sxmo_log "ERR: callaudiocli -u 0 failed"
 }
 
 is_call_audio_mode() {
@@ -68,11 +69,11 @@ is_default_audio_mode() {
 }
 
 enable_call_audio_mode() {
-	callaudiocli -m 1
+	callaudiocli -m 1 || sxmo_log "ERR: callaudiocl -m 1 failed"
 }
 
 disable_call_audio_mode() {
-	callaudiocli -m 0
+	callaudiocli -m 0 || sxmo_log "ERR: callaudiocli -m 0 failed"
 }
 
 is_enabled_speaker() {
@@ -84,11 +85,11 @@ is_disabled_speaker() {
 }
 
 enable_speaker() {
-	callaudiocli -s 1
+	callaudiocli -s 1 || sxmo_log "ERR: callaudiocli -s 1 failed"
 }
 
 disable_speaker() {
-	callaudiocli -s 0
+	callaudiocli -s 0 || sxmo_log "ERR: callaudiocli -s 0 failed"
 }
 
 "$@"
