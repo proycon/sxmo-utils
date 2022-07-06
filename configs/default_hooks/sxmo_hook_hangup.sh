proycon/sxmo-utils

commit 317bf34f2e77e93b08b6205641514d794301cb88

Add a hangup hook when you hangup the call

configs/default_hooks/sxmo_hook_hangup.sh
@@ -1,0 +1,6 @@
+#!/bin/sh
+
+# This script is executed (asynchronously) when you discard an incoming call
+
+# just do the same as the missed_call hook:
+exec sxmo_hook_missed_call.sh "$@"

scripts/modem/sxmo_modemcall.sh
@@ -73,7 +73,10 @@ hangup() {
 	if [ -f "$XDG_RUNTIME_DIR/${CALLID}.pickedupcall" ]; then
 		rm -f "$XDG_RUNTIME_DIR/${CALLID}.pickedupcall"
 		touch "$XDG_RUNTIME_DIR/${CALLID}.hangedupcall"
-		log_event "call_hangup" "$CALLID"
+		log_event "call_hangsxmo_hook_discard.shup" "$CALLID"
+
+		sxmo_log "sxmo_modemcall: Invoking hangup hook (async)"
+		sxmo_hook_hangup.sh &
 	else
 		#this call was never picked up and hung up immediately, so it is a discarded call
 		touch "$XDG_RUNTIME_DIR/${CALLID}.discardedcall"

commit 6d348486358e283add203fd1a8cc2a006b875628

fix sxmo_hook_hangup
317bf34f2e77e93b08b6205641514d794301cb88 introduced a small typo in the
comment.  Also the 'hangup' hook should *not* call the missed call hook,
since these are in principle distinct.  (On my machine, for instance, I
have missed calls e-mailed to me; but I don't want hangup calls e-mailed
to me.)  (See also sxmo_hook_discard.sh)

Signed-off-by: Stacy Harper <contact@stacyharper.net>

configs/default_hooks/sxmo_hook_hangup.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# This script is executed (asynchronously) when you discard an incoming call
-
-# just do the same as the missed_call hook:
-exec sxmo_hook_missed_call.sh "$@"
+# This script is executed (asynchronously) when you hangup an incoming call

scripts/modem/sxmo_modemcall.sh
@@ -73,7 +73,7 @@ hangup() {
 	if [ -f "$XDG_RUNTIME_DIR/${CALLID}.pickedupcall" ]; then
 		rm -f "$XDG_RUNTIME_DIR/${CALLID}.pickedupcall"
 		touch "$XDG_RUNTIME_DIR/${CALLID}.hangedupcall"
-		log_event "call_hangsxmo_hook_discard.shup" "$CALLID"
+		log_event "call_hangup" "$CALLID"
 
 		sxmo_log "sxmo_modemcall: Invoking hangup hook (async)"
 		sxmo_hook_hangup.sh &