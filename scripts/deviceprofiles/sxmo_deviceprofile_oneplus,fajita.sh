6b950ed1fc06e81ffe107bb05cddc2faf0bd708a
Joshua Murphy <joshuamurphy@posteo.net>
2022-07-10 18:38:24 -0400

    Add deviceprofile for 'oneplus,fajita'
    Signed-off-by: Maarten van Gompel <proycon@anaproy.nl>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_oneplus,fajita.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+export SXMO_VOLUME_BUTTON="1:1:Volume_keys"
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_MONITOR="1739:0:Synaptics_S3706B"
