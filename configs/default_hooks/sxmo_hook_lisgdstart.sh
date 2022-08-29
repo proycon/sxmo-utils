39157a961523635551405ef32b29145a9a348764
Stacy Harper <contact@stacyharper.net>
2022-08-29 21:06:49 +0200

    Free lisgd/bonsaid process logs from forks logs
    Signed-off-by: Stacy Harper <contact@stacyharper.net>
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

--- configs/default_hooks/sxmo_hook_lisgdstart.sh
+++ configs/default_hooks/sxmo_hook_lisgdstart.sh
@@ -24,45 +24,45 @@ fi
 #order matters, only the first match gets executed
 lisgd "$@" -d "$LISGD_INPUT_DEVICE" ${orientation:+-o $orientation} \
 	-t "$LISGD_THRESHOLD" -T "$LISGD_THRESHOLD_PRESSED" \
-	-g "1,DRUL,BR,*,setsid -f sxmo_hook_inputhandler.sh bottomrightcorner" \
-	-g "1,DLUR,BL,*,setsid -f sxmo_hook_inputhandler.sh bottomleftcorner" \
-	-g "1,ULDR,TL,*,setsid -f sxmo_hook_inputhandler.sh topleftcorner" \
-	-g "1,URDL,TR,*,setsid -f sxmo_hook_inputhandler.sh toprightcorner" \
-	-g "1,LR,B,L,setsid -f sxmo_hook_inputhandler.sh rightbottomedge" \
-	-g "1,RL,B,L,setsid -f sxmo_hook_inputhandler.sh leftbottomedge" \
-	-g "1,LR,L,*,setsid -f sxmo_hook_inputhandler.sh rightleftedge" \
-	-g "1,RL,R,*,setsid -f sxmo_hook_inputhandler.sh leftrightedge" \
-	-g "1,DU,L,*,P,setsid -f sxmo_hook_inputhandler.sh upleftedge" \
-	-g "1,UD,L,*,P,setsid -f sxmo_hook_inputhandler.sh downleftedge" \
-	-g "1,LR,T,*,P,setsid -f sxmo_hook_inputhandler.sh righttopedge" \
-	-g "1,RL,T,*,P,setsid -f sxmo_hook_inputhandler.sh lefttopedge" \
-	-g "1,DU,B,*,setsid -f sxmo_hook_inputhandler.sh upbottomedge" \
-	-g "1,UD,B,*,setsid -f sxmo_hook_inputhandler.sh downbottomedge" \
-	-g "1,UD,T,*,setsid -f sxmo_hook_inputhandler.sh downtopedge" \
-	-g "1,DU,T,*,setsid -f sxmo_hook_inputhandler.sh uptopedge" \
-	-g "2,UD,T,*,setsid -f sxmo_hook_inputhandler.sh twodowntopedge" \
-	-g "2,UD,B,*,setsid -f sxmo_hook_inputhandler.sh twodownbottomedge" \
-	-g "1,DU,R,*,P,setsid -f sxmo_hook_inputhandler.sh uprightedge" \
-	-g "1,UD,R,*,P,setsid -f sxmo_hook_inputhandler.sh downrightedge" \
-	-g "1,LR,R,S,setsid -f sxmo_hook_inputhandler.sh rightrightedge_short" \
-	-g "1,RL,L,S,setsid -f sxmo_hook_inputhandler.sh leftrightedge_short" \
-	-g "1,RL,*,L,setsid -f sxmo_hook_inputhandler.sh longoneleft" \
-	-g "1,LR,*,L,setsid -f sxmo_hook_inputhandler.sh longoneright" \
-	-g "1,DU,*,L,setsid -f sxmo_hook_inputhandler.sh longoneup" \
-	-g "1,UD,*,L,setsid -f sxmo_hook_inputhandler.sh longonedown" \
-	-g "1,RL,*,M,setsid -f sxmo_hook_inputhandler.sh mediumoneleft" \
-	-g "1,LR,*,M,setsid -f sxmo_hook_inputhandler.sh mediumoneright" \
-	-g "1,DU,*,M,setsid -f sxmo_hook_inputhandler.sh mediumoneup" \
-	-g "1,UD,*,M,setsid -f sxmo_hook_inputhandler.sh mediumonedown" \
-	-g "1,RL,*,*,setsid -f sxmo_hook_inputhandler.sh oneleft" \
-	-g "1,LR,*,*,setsid -f sxmo_hook_inputhandler.sh oneright" \
-	-g "1,DU,*,*,setsid -f sxmo_hook_inputhandler.sh oneup" \
-	-g "1,UD,*,*,setsid -f sxmo_hook_inputhandler.sh onedown" \
-	-g "1,DRUL,*,*,setsid -f sxmo_hook_inputhandler.sh upleft" \
-	-g "1,URDL,*,*,setsid -f sxmo_hook_inputhandler.sh downleft" \
-	-g "1,DLUR,*,*,setsid -f sxmo_hook_inputhandler.sh upright" \
-	-g "1,ULDR,*,*,setsid -f sxmo_hook_inputhandler.sh downright" \
-	-g "2,RL,*,*,setsid -f sxmo_hook_inputhandler.sh twoleft" \
-	-g "2,LR,*,*,setsid -f sxmo_hook_inputhandler.sh tworight" \
-	-g "2,DU,*,*,setsid -f sxmo_hook_inputhandler.sh twoup" \
-	-g "2,UD,*,*,setsid -f sxmo_hook_inputhandler.sh twodown"
+	-g "1,DRUL,BR,*,setsid -f sxmo_hook_inputhandler.sh bottomrightcorner >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DLUR,BL,*,setsid -f sxmo_hook_inputhandler.sh bottomleftcorner >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,ULDR,TL,*,setsid -f sxmo_hook_inputhandler.sh topleftcorner >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,URDL,TR,*,setsid -f sxmo_hook_inputhandler.sh toprightcorner >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,LR,B,L,setsid -f sxmo_hook_inputhandler.sh rightbottomedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,RL,B,L,setsid -f sxmo_hook_inputhandler.sh leftbottomedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,LR,L,*,setsid -f sxmo_hook_inputhandler.sh rightleftedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,RL,R,*,setsid -f sxmo_hook_inputhandler.sh leftrightedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DU,L,*,P,setsid -f sxmo_hook_inputhandler.sh upleftedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,UD,L,*,P,setsid -f sxmo_hook_inputhandler.sh downleftedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,LR,T,*,P,setsid -f sxmo_hook_inputhandler.sh righttopedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,RL,T,*,P,setsid -f sxmo_hook_inputhandler.sh lefttopedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DU,B,*,setsid -f sxmo_hook_inputhandler.sh upbottomedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,UD,B,*,setsid -f sxmo_hook_inputhandler.sh downbottomedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,UD,T,*,setsid -f sxmo_hook_inputhandler.sh downtopedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DU,T,*,setsid -f sxmo_hook_inputhandler.sh uptopedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "2,UD,T,*,setsid -f sxmo_hook_inputhandler.sh twodowntopedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "2,UD,B,*,setsid -f sxmo_hook_inputhandler.sh twodownbottomedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DU,R,*,P,setsid -f sxmo_hook_inputhandler.sh uprightedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,UD,R,*,P,setsid -f sxmo_hook_inputhandler.sh downrightedge >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,LR,R,S,setsid -f sxmo_hook_inputhandler.sh rightrightedge_short >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,RL,L,S,setsid -f sxmo_hook_inputhandler.sh leftrightedge_short >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,RL,*,L,setsid -f sxmo_hook_inputhandler.sh longoneleft >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,LR,*,L,setsid -f sxmo_hook_inputhandler.sh longoneright >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DU,*,L,setsid -f sxmo_hook_inputhandler.sh longoneup >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,UD,*,L,setsid -f sxmo_hook_inputhandler.sh longonedown >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,RL,*,M,setsid -f sxmo_hook_inputhandler.sh mediumoneleft >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,LR,*,M,setsid -f sxmo_hook_inputhandler.sh mediumoneright >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DU,*,M,setsid -f sxmo_hook_inputhandler.sh mediumoneup >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,UD,*,M,setsid -f sxmo_hook_inputhandler.sh mediumonedown >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,RL,*,*,setsid -f sxmo_hook_inputhandler.sh oneleft >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,LR,*,*,setsid -f sxmo_hook_inputhandler.sh oneright >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DU,*,*,setsid -f sxmo_hook_inputhandler.sh oneup >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,UD,*,*,setsid -f sxmo_hook_inputhandler.sh onedown >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DRUL,*,*,setsid -f sxmo_hook_inputhandler.sh upleft >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,URDL,*,*,setsid -f sxmo_hook_inputhandler.sh downleft >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,DLUR,*,*,setsid -f sxmo_hook_inputhandler.sh upright >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "1,ULDR,*,*,setsid -f sxmo_hook_inputhandler.sh downright >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "2,RL,*,*,setsid -f sxmo_hook_inputhandler.sh twoleft >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "2,LR,*,*,setsid -f sxmo_hook_inputhandler.sh tworight >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "2,DU,*,*,setsid -f sxmo_hook_inputhandler.sh twoup >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1" \
+	-g "2,UD,*,*,setsid -f sxmo_hook_inputhandler.sh twodown >> \"$XDG_STATE_HOME\"/lisgd_execs.log 2>&1"
