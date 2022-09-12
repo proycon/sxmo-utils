c769ef15182fd6092e115266754e74c39c4f7631
Stacy Harper <contact@stacyharper.net>
2022-09-12 12:55:35 +0200

    Allow multikeys without bonsaictl

--- scripts/core/sxmo_swayinitconf.sh
+++ scripts/core/sxmo_swayinitconf.sh
@@ -13,6 +13,35 @@ pwr="${SXMO_POWER_BUTTON:-"0:0:axp20x-pek"}"
 vols="${SXMO_VOLUME_BUTTON:-"1:1:1c21800.lradc"}"
 scale="${SXMO_SWAY_SCALE:-2}"
 
+# Drop this when bonsai is available on 32 bits systems
+multikey_retrocompat() {
+	sxmo_multikey.sh clear
+
+	swaymsg -- bindsym --locked --input-device="$pwr" XF86PowerOff exec sxmo_multikey.sh \
+		powerbutton \
+		powerbutton_one \
+		powerbutton_two \
+		powerbutton_three
+
+	 if ! [ "$vols" = "none" ]; then
+		for vol in $vols; do
+			swaymsg -- bindsym --locked --input-device="$vol" XF86AudioRaiseVolume exec \
+				sxmo_multikey.sh \
+				volup \
+				volup_one \
+				volup_two \
+				volup_three
+
+			swaymsg -- bindsym --locked --input-device="$vol" XF86AudioLowerVolume exec \
+				sxmo_multikey.sh \
+				voldown \
+				voldown_one \
+				voldown_two \
+				voldown_three
+		done
+	 fi
+}
+
 if [ -n "$SXMO_MODEM_GPIO_KEY_RI" ]; then
 	# Disable the gpio-key-ri input devive
 	# It will trigger idle wakeup on modem notification which break sxmo
@@ -35,6 +64,11 @@ if ! [ "$vols" = "none" ]; then
 		swaymsg -- input "$vol" repeat_rate 15
 		swaymsg -- input "$vol" xkb_file "$(xdg_data_path sxmo/sway/xkb_mobile_normal_buttons)"
 	done
+fi
+
+if ! command -v bonsaictl > /dev/null; then
+	multikey_retrocompat
+	exit
 fi
 
 swaymsg -- bindsym --locked --input-device="$pwr" --no-repeat XF86PowerOff exec bonsaictl -e power_pressed
