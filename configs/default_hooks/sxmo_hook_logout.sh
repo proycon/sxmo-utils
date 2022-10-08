d90dcf4131aa4295c6b170dad3e9c5f17470fe79
Peter John Hartman <peterjohnhartman@gmail.com>
2022-10-07 20:35:23 -0500

    logout: fix type

--- configs/default_hooks/sxmo_hook_logout.sh
+++ configs/default_hooks/sxmo_hook_logout.sh
@@ -3,7 +3,7 @@
 # current window manager.  You'll want to give a 5s pause here to let certain apps
 # `cool off' before we restart wm, but otherwise everything *should* die with the 
 # wm
-superctl stop sxmo_sound_monitor
+superctl stop sxmo_soundmonitor
 superctl stop wireplumber
 superctl stop pipewire-pulse
 superctl stop pipewire
