937f7ea0f0c75174665092124e4d3f7068573628
Anjandev Momi <anjan@momi.ca>
2022-10-16 20:40:33 -0700

    oneplus,enchilada.sh: add monitor, vibrate dev, and disable led

--- scripts/deviceprofiles/sxmo_deviceprofile_oneplus,enchilada.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_oneplus,enchilada.sh
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright 2022 Sxmo Contributors
 
-# TODO: TESTME!
 export SXMO_VOLUME_BUTTON="1:1:Volume_keys"
 export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_MONITOR="1739:0:Synaptics_S3706B"
+export SXMO_DISABLE_LEDS="1"
+export SXMO_VIBRATE_DEV="/dev/input/by-path/platform-c440000.spmi-platform-c440000.spmi:pmic@3:haptics@c000-event"
