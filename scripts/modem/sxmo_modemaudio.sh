64879b74a06d16e08b2d07f78abc8ee8509c3298
ArenM <aren@peacevolution.org>
2022-10-28 13:55:16 -0400

    modemaudio: don't start callaudiod
    This is handled by dbus automatically. And the way it's done here causes
    the script to hang if callaudiod hasn't been started yet.
    
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/modem/sxmo_modemaudio.sh
+++ scripts/modem/sxmo_modemaudio.sh
@@ -64,7 +64,6 @@ is_default_audio_mode() {
 }
 
 enable_call_audio_mode() {
-	pgrep -f callaudiod || callaudiod
 	dbus-send --session --print-reply --type=method_call --dest=org.mobian_project.CallAudio \
 		/org/mobian_project/CallAudio org.mobian_project.CallAudio.SelectMode uint32:1 >/dev/null
 
@@ -78,7 +77,6 @@ enable_call_audio_mode() {
 }
 
 disable_call_audio_mode() {
-	pgrep -f callaudiod || callaudiod
 	dbus-send --session --print-reply --type=method_call --dest=org.mobian_project.CallAudio \
 		/org/mobian_project/CallAudio org.mobian_project.CallAudio.SelectMode uint32:0 >/dev/null
 
