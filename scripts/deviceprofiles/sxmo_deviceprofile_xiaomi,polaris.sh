97734923c214795fd97db9e2190fd869376b51cc
Philipp Schmitt <philipp@schmitt.co>
2022-12-05 18:14:40 +0100

    polaris: add device profile
    Signed-off-by: Philipp Schmitt <philipp@schmitt.co>
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_xiaomi,polaris.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# mi mix 2s
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+export SXMO_DISABLE_LEDS="1"
+export SXMO_MONITOR="1739:0:Synaptics_S3330"
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_VOLUME_BUTTON="1:1:gpio-keys 0:0:pm8941_resin"
+export SXMO_SWAY_SCALE="2"
