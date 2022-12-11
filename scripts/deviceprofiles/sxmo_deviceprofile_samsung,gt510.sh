341381fa5c57d79bcaa5cc13ce0766a93999984b
xengineering <me@xengineering.eu>
2022-12-11 15:32:14 +0100

    Fix scale for samsung,gt510
    The currently used scale is far too big. This commit decreases it from
    2.0 to 1.5 which looks good on the device.
    
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/deviceprofiles/sxmo_deviceprofile_samsung,gt510.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_samsung,gt510.sh
@@ -6,4 +6,4 @@
 export SXMO_DISABLE_LEDS="1"
 export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
 export SXMO_VOLUME_BUTTON="1:1:GPIO_Buttons 0:0:pm8941_resin"
-export SXMO_SWAY_SCALE="2"
+export SXMO_SWAY_SCALE="1.5"
