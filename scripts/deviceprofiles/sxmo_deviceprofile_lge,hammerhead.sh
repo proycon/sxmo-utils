968edf20fd2cf7c25a097cb8f7e1fe908457f5e5
HexagonWin <hexagonwin@disroot.org>
2022-12-17 21:09:53 +0900

    Fix volume keys for lge,hammerhead
    The current values are not working as of kernel 5.19, so we update the
    values.
    
    Signed-off-by: HexagonWin <hexagonwin@disroot.org>
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/deviceprofiles/sxmo_deviceprofile_lge,hammerhead.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_lge,hammerhead.sh
@@ -4,7 +4,7 @@
 export SXMO_DISABLE_LEDS="1" # Not supported by kernel (as of 5.16).
 export SXMO_MIN_BRIGHTNESS="4" # Screen turns off below 4.
 export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
-export SXMO_VOLUME_BUTTON="1:1:soc:gpio-keys"
+export SXMO_VOLUME_BUTTON="1:1:gpio-keys"
 
 export SXMO_MODEMRTC="9999" # No Modem RTC
 export SXMO_POWERRTC="11" # "battery"
