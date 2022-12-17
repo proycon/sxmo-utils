c34ee328eca8aa56224cd199a54ff676b08d161e
Joshua Murphy <joshuamurphy@posteo.net>
2022-12-17 21:39:27 +0000

    Fix monitor for fajita
    This patch supersedes my previous patch for fixing just the scale value.
    
    Signed-off-by: Stacy Harper <contact@stacyharper.net>

--- scripts/deviceprofiles/sxmo_deviceprofile_oneplus,fajita.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_oneplus,fajita.sh
@@ -5,7 +5,6 @@
 export SXMO_DISABLE_LEDS="1"
 export SXMO_VOLUME_BUTTON="1:1:Volume_keys"
 export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
-# TODO: change to output display
-export SXMO_MONITOR="1739:0:Synaptics_S3706B"
+export SXMO_MONITOR="DSI-1"
 export SXMO_VIBRATE_DEV="/dev/input/by-path/platform-c440000.spmi-platform-c440000.spmi:pmic@3:haptics@c000-event"
 export SXMO_SWAY_SCALE="3"
