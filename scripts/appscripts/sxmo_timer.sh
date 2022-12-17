088feda2c81892700dfd502a30e74183a2c23129
Stacy Harper <contact@stacyharper.net>
2022-12-17 11:21:27 +0100

    Cleanup sxmo_timer.sh

--- scripts/appscripts/sxmo_timer.sh
+++ scripts/appscripts/sxmo_timer.sh
@@ -7,20 +7,20 @@
 . sxmo_common.sh
 
 timerrun() {
-	TIME=$(
+	TIME="$(
 		echo "$@" |
 		sed 's/\([^0-9]\)\([0-9]\)/\1+\2/g; s/h/*60m/g; s/m/*60s/g; s/s//g' |
 		bc
-	)
+	)"
 
-	DATE1=$(($(date +%s) + TIME));
+	DATE1="$(($(date +%s) + TIME))";
 	while [ "$DATE1" -ge "$(date +%s)" ]; do
-		printf %b "$(date -u --date @$((DATE1 - $(date +%s))) +%H:%M:%S) \r";
+		printf "%s\r" "$(date -u --date @$((DATE1 - $(date +%s))) +%H:%M:%S)";
 		sleep 0.1
 	done
 	echo "Done with $*"
 
-	while :;
+	while : ;
 		do notify-send  "Done with $*";
 		sxmo_vibrate 1000
 		sleep 0.5
@@ -29,7 +29,7 @@ timerrun() {
 
 stopwatchrun() {
 	start="$(date +%s)"
-	while true; do
+	while : ; do
 		time="$(($(date +%s) - start))"
 		printf '%s\r' "$(date -u -d "@$time" +%H:%M:%S)"
 	done
@@ -37,33 +37,39 @@ stopwatchrun() {
 }
 
 menu() {
-	TIMEINPUT="$(
-		echo "
-			Stopwatch
-			1h
-			10m
-			9m
-			8m
-			7m
-			6m
-			5m
-			4m
-			3m
-			2m
-			1m
-			30s
-			Close Menu
-		" | awk 'NF' | awk '{$1=$1};1' | sxmo_dmenu_with_kb.sh -p Timer
-	)" || exit 0
+	TIMEINPUT="$(sxmo_dmenu_with_kb.sh -p Timer <<EOF
+Stopwatch
+1h
+10m
+9m
+8m
+7m
+6m
+5m
+4m
+3m
+2m
+1m
+30s
+Close Menu
+EOF
+	)" || exit
 	case "$TIMEINPUT" in
-	"Close Menu") exit 0 ;;
-	"Stopwatch")
-		sxmo_terminal.sh "$0" stopwatchrun
-		;;
-	*)
-		sxmo_terminal.sh "$0" timerrun "$TIMEINPUT"
-		;;
+		"Close Menu")
+			exit 0
+			;;
+		"Stopwatch")
+			sxmo_terminal.sh "$0" stopwatchrun
+			;;
+		*)
+			sxmo_terminal.sh "$0" timerrun "$TIMEINPUT"
+			;;
 	esac
 }
 
-if [ $# -gt 0 ]; then "$@"; else menu; fi
+if [ $# -gt 0 ]
+then
+	"$@"
+else
+	menu
+fi
