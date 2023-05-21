8691177ba57fd8be307546d20d43079a35010a86
René Kliment <rene@renekliment.cz>
2023-05-21 13:46:51 +0200

    add initial support for samsung,n8010
    This makes the power, volume down & volume up buttons work in SXMO
    on my Samsung Galaxy Note 10.1 (samsung-p4note), the N8010 variant.
    
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_samsung,n8010.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+
+# Samsung Galaxy Note 10.1 2012 (samsung-p4note), N8010 (WiFi-only)
+
+export SXMO_POWER_BUTTON="1:1:gpio-keys"
+export SXMO_VOLUME_BUTTON="1:1:gpio-keys"
