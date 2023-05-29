567331ab0b12a2021605ac4fc1bb6a2678daffa7
Anjandev Momi <anjan@momi.ca>
2023-05-29 02:01:44 -0400

    sxmo_hook_apps.sh: add geary

--- configs/default_hooks/sxmo_hook_apps.sh
+++ configs/default_hooks/sxmo_hook_apps.sh
@@ -22,6 +22,7 @@ write_line_app() {
 
 write_line_app jami-qt "$icon_msg Jami" "jami-qt"
 write_line_app aerc "$icon_eml Aerc" "sxmo_terminal.sh aerc"
+write_line_app geary "$icon_eml Geary" "geary"
 write_line_app amfora "$icon_glb Amfora" "sxmo_terminal.sh amfora"
 write_line_app alpine "$icon_eml Alpine" "sxmo_terminal.sh alpine"
 write_line_app anbox-launch "$icon_and Anbox" "anbox"
