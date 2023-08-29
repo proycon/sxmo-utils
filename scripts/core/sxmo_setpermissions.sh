871caa7394d2deda6aaff89af0fea9f6de773c2e
Willow Barraco <contact@willowbarraco.fr>
2023-08-29 23:33:17 +0200

    Revert "Remove dead code from setpermissions"
    This reverts commit da36357b4f2eea6f86aa9823b8c5a4f505b94b12.

--- scripts/core/sxmo_setpermissions.sh
+++ scripts/core/sxmo_setpermissions.sh
@@ -18,6 +18,6 @@ fi
 # users can override this in sxmo_deviceprofile_mydevice.sh
 files="${SXMO_SYS_FILES:-"/sys/power/state /sys/power/mem_sleep /dev/rtc0"}"
 
-for file in $files; do
+for file in $files /sys/power/wakeup_count; do
 	[ -e "$file" ] && chmod a+rw "$file"
 done
