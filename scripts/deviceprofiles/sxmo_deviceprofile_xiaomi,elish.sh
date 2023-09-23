7a7188bddcc8ac45eeea7748bc342b390b48f0f5
Anjandev Momi <anjan@momi.ca>
2023-09-22 18:33:13 -0700

    xiaomi elish: set MONITOR to DSI-1
    https://lists.sr.ht/~mil/sxmo-devel/%3C20230807161613.13072-2-noneofyourbusiness%40danwin1210.de%3E#%3C20230903124519.486585ca@workstation.localdomain%3E

--- scripts/deviceprofiles/sxmo_deviceprofile_xiaomi,elish.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_xiaomi,elish.sh
@@ -4,7 +4,7 @@
 # Copyright 2022 Sxmo Contributors
 
 export SXMO_DISABLE_LEDS="1"
-export SXMO_MONITOR="0:0:NVTCapacitiveTouchScreen"
+export SXMO_MONITOR="DSI-1"
 export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
 export SXMO_VOLUME_BUTTON="1:1:gpio-keys 0:0:pm8941_resin"
 #export SXMO_SWAY_SCALE=""
