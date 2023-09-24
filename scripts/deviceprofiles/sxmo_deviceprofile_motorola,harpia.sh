df539391e50cd7c6520b5e9f7c47573a3d1579c6
Paavo Ylönen <paavo@paavo.guide>
2023-09-24 17:40:57 +0300

    Deviceprofile for Motorola Moto G4 Play

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_motorola,harpia.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Motorola Moto G4 Play
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+# Bragging rights Paavo Ylönen
+
+export SXMO_DISABLE_LEDS="0"
+export SXMO_MONITOR="DSI-1"
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_VOLUME_BUTTON="1:1:gpio-keys 0:0:pm8941_resin"
