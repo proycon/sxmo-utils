e99ef4e2e377188b093855c194fb1c4135283e97
Jan Wagemakers <sourcehut@janwagemakers.be>
2023-09-29 18:08:09 +0200

    add SXMO_VIBRATE_DEV to SHIFT6mq deviceprofile
    This makes vibrate work on the shiftphone shift6mq.
    
    Signed-off-by: Willow Barraco <contact@willowbarraco.fr>

--- scripts/deviceprofiles/sxmo_deviceprofile_shift,axolotl.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_shift,axolotl.sh
@@ -3,3 +3,4 @@
 export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
 export SXMO_VOLUME_BUTTON="1:1:gpio-keys 0:0:pm8941_resin"
 export SXMO_SWAY_SCALE="2.5"
+export SXMO_VIBRATE_DEV="/dev/input/by-path/platform-c440000.spmi-platform-c440000.spmi:pmic@3:haptics@c000-event"
