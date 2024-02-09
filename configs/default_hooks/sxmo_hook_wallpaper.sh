c6efaf8a940880370beecbde6f974e5b9f986aad
Willow Barraco <contact@willowbarraco.fr>
2024-02-09 09:09:31 +0100

    Support system wallpapers
    This add a simple hook "wallpaper" that by default grab the system
    wallpaper (ex: provided by postmarketos-artwork-wallpapers). If there is
    none, it fallback to the sxmo one.
    
    Of course, the hook is customizable by the user if needed.
    
    Ref: https://gitlab.com/postmarketOS/pmaports/-/issues/2499
    Signed-off-by: Willow Barraco <contact@willowbarraco.fr>
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

---
+++ configs/default_hooks/sxmo_hook_wallpaper.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+# shellcheck source=scripts/core/sxmo_common.sh
+. sxmo_common.sh
+
+# This script is executed once, and must output the wallpaper to display
+
+installed_wallpapers() {
+	xdg_data_path wallpapers 0 "\0" | xargs -r0I{} find "{}" -name "$SXMO_OS.*"
+}
+
+sxmo_wallpaper() {
+	xdg_data_path sxmo/background.jpg
+}
+
+all_wallpapers() {
+	installed_wallpapers
+	sxmo_wallpaper
+}
+
+all_wallpapers | head -n1
