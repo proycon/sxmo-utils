fb153ce98756e051961ef2d6f66ad67d9761b87e
Caio Barros <caio.barros@gmail.com>
2024-02-10 19:07:57 -0300

    Initial setup of a device profile for Pine Tab 2
    Copied from the Pine Phone Pro profile, but (1) removed the modem, (2)
    removed wayland scaling and (3) changed the power key to 0:0:rk805_pwrkey
    
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_pine64,pinetab2-v2.0.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2022 Sxmo Contributors
+
+export SXMO_TOUCHSCREEN_ID=7
+export SXMO_MONITOR="DSI-1"
+export SXMO_POWER_BUTTON="0:0:rk805_pwrkey"
+export SXMO_VOLUME_BUTTON="1:1:adc-keys"
