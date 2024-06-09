27da98032fba8698975bb5e14e7921d809f8b4fc
Sicelo A. Mhlongo <absicsz@gmail.com>
2024-06-09 09:13:08 +0200

    omap3-n900: set number of lines in dmenu
    default dmenu lines are too long for N900's tiny screen. reduce to fit
    
    Signed-off-by: Sicelo A. Mhlongo <absicsz@gmail.com>
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

--- scripts/deviceprofiles/sxmo_deviceprofile_nokia,omap3-n900.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_nokia,omap3-n900.sh
@@ -9,6 +9,8 @@ export SXMO_SWAY_SCALE="1.5"
 #on N900 these two are swaped
 export SXMO_BEMENU_LANDSCAPE_LINES="10"
 export SXMO_BEMENU_PORTRAIT_LINES="8"
+export SXMO_DMENU_LANDSCAPE_LINES="10"
+export SXMO_DMENU_PORTRAIT_LINES="6"
 export SXMO_ROTATE_DIRECTION="left"
 export SXMO_KEYBOARD_SLIDER_EVENT_DEVICE="/dev/input/by-path/platform-gpio_keys-event"
 export SXMO_KEYBOARD_SLIDER_CLOSE_EVENT="*code 10 (SW_KEYPAD_SLIDE), value 0*"
