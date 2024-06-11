6db78c48624efaec089a1b444334978941525ec8
Willow Barraco <contact@willowbarraco.fr>
2024-06-11 08:05:41 +0200

    hook_call_audio: drop back and forth hack on mic and speaker
    Signed-off-by: Willow Barraco <contact@willowbarraco.fr>
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

--- configs/default_hooks/sxmo_hook_call_audio.sh
+++ configs/default_hooks/sxmo_hook_call_audio.sh
@@ -9,27 +9,3 @@
 
 # shellcheck source=scripts/core/sxmo_common.sh
 . sxmo_common.sh
-
-# Phonecall started
-if [ "$1" = "enable" ]; then
-	sxmo_log "Attempting hack to get things just right."
-	# fixes bug where sometimes we start with speaker on and mic off
-	sxmo_modemaudio.sh enable_speaker
-	sxmo_modemaudio.sh disable_speaker
-	sxmo_modemaudio.sh mute_mic
-	sxmo_modemaudio.sh unmute_mic
-
-	# Add other things here, e.g., volume boosters
-
-	sxmo_modemaudio.sh is_disabled_speaker && sxmo_modemaudio.sh is_unmuted_mic
-# Phonecall ended
-elif [ "$1" = "disable" ]; then
-	sxmo_log "Attempting hack to get things just right."
-	# fixes bug where sometimes we leave call with speaker off
-	sxmo_modemaudio.sh disable_speaker
-	sxmo_modemaudio.sh enable_speaker
-
-	# Add other things here, e.g., volume boosters
-
-	sxmo_modemaudio.sh is_enabled_speaker
-fi
