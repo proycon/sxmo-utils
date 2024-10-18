516c6d25db16be4048bb663e1cfc1ab625a655a7
Maarten van Gompel <proycon@anaproy.nl>
2024-10-18 22:37:24 +0200

    Added deviceprofile for Google Pixel 3a (google-sargo)
    Co-authored-by: fdelamotte

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_google,b4s4-sdm670.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2024 Sxmo Contributors
+
+export SXMO_VOLUME_BUTTON="1:1:gpio-keys 0:0:pm8941_resin"
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_VIBRATE_DEV="/dev/input/event2"
+export SXMO_VIBRATE_STRENGTH="50000"
+export SXMO_SWAY_SCALE="3"
