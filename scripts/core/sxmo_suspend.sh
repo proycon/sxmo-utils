921fa1e24c047ac0470755146b12a2d97fd0c6b3
Maarten van Gompel <proycon@anaproy.nl>
2024-10-28 13:57:00 +0100

    Prevent HexagonRPCD from freezing device after wake
    This solves the following issue on a Google Pixel 3a:
    * https://gitlab.postmarketos.org/postmarketOS/pmaports/-/issues/3052
    * https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/5400
    
    Since we don't use elogind for suspend, we have to handle this logic
    ourselves.
    
    Though I'm not fond of device-specific code in our main scripts, the
    generic solution wasn't really recommended.
    
    Signed-off-by: Aren Moynihan <aren@peacevolution.org>

--- scripts/core/sxmo_suspend.sh
+++ scripts/core/sxmo_suspend.sh
@@ -21,6 +21,26 @@ if [ -n "$mnc" ] && [ "$mnc" -gt 0 ] && [ "$mnc" -lt "$YEARS8_TO_SEC" ]; then
 	fi
 fi
 
+
+if [ "$SXMO_DEVICE_NAME" = "google,b4s4-sdm670" ]; then #(google pixel 3a)
+	# Quoting Richard Acayan from: https://gitlab.postmarketos.org/postmarketOS/pmaports/-/merge_requests/5400/ :
+	#
+	# There is a bug in FastRPC when waking from suspend. Since HexagonRPCD is
+	# currently only useful for a few moments when the ADSP is requesting the
+	# sensor registry, it can just be stopped without affecting sensor
+	# support. Add a pre-suspend hook to stop HexagonRPCD so it doesn't crash
+	# the ADSP when the device wakes up.
+	#
+	case "$SXMO_OS" in
+		alpine|postmarketos)
+			doas -n rc-service hexagonrpcd-adsp-sensorspd stop
+			;;
+		arch|archarm|nixos|debian)
+			doas -n systemctl stop hexagonrpcd-adsp-sensorspd
+			;;
+	esac
+fi
+
 sxmo_log "calling suspend with suspend_time <$suspend_time>"
 
 start="$(date "+%s")"
